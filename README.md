1cd7655b0b47df83cbbbab8d2fad307b29385535
Jaakko-Heikki Heusala <jheusala@iki.fi>
2014-11-28 10:33:57 +0200

    Improved documentation

--- README.md
+++ README.md
@@ -2,3 +2,26 @@ nor-function
 ============
 
 Function utilities for JavaScript and Node.js
+
+Usage
+-----
+
+Install from NPM: `npm install -d nor-function`
+
+### Serialize function to String
+
+```
+var FUNCTION = require("nor-function");
+function foo(a, b) { return a + b; }
+var str = FUNCTION.toString(foo);
+str === "function foo(a, b) { return a + b; }";
+```
+
+### Unserialize string to function
+
+```
+var FUNCTION = require("nor-function");
+var str = "function foo(a, b) { return a + b; }";
+var fun = FUNCTION.toFunction(str);
+fun(10, 20) === 30;
+```
--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@
 //var debug = require('nor-debug');
 var fun = module.exports = {};
 
-/** Serialize JavaScript function as string */
+/** Serialize JavaScript function as string.
+ * @param f {function} The function to be serialized.
+ * @returns {string} The function serialized to JavaScript string.
+ */
 fun.toString = function(f) {
 	if(!(f && (f instanceof Function))) {
 		throw new TypeError("argument is not Function");
@@ -18,7 +21,10 @@ fun.toString = function(f) {
 	return s;
 };
 
-/** Covert stringified function to JavaScript function */
+/** Covert stringified function to JavaScript function.
+ * @param s {string} The string containing code for JavaScript function.
+ * @returns {function} The unserialized JavaScript function.
+ */
 fun.toFunction = function(s) {
 	s = ''+s;
 
