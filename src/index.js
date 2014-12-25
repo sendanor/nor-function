756686df6ceb42b045001ef24db9ecffbef97a20
Jaakko-Heikki Heusala <jheusala@iki.fi>
2014-12-26 01:05:07 +0200

    Implemented abind()

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 "use strict";
 
 var bind = require('./bind.js');
+var abind = require('./abind.js');
 var curry = require('./curry.js');
 var stringify = require('./stringify.js');
 var parse = require('./parse.js');
@@ -10,6 +11,7 @@ var parseName = require('./parseName.js');
 function FUNCTION(f) {
 	return {
 		"bind": bind(f),
+		"abind": abind(f),
 		"curry": curry(f),
 		"stringify": function() {
 			return stringify(f);
